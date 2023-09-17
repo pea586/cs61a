38db596ec044c4dbbdd14fa9c208b40097973ca7
Bill <keepgoingbill@gmail.com>
2023-09-17 18:37:44 +1000

    ' Invoking Doctest'

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -1,11 +1,20 @@
-def twenty_twenty_two():
+def twenty_twenty_two(a):
     """Come up with the most creative expression that evaluates to 2022,
     using only numbers and the +, *, and - operators.
 
-    ''>>> twenty_twenty_two()
-    2022
+    >>> ans = twenty_twenty_two(12)
+    >>> ans
+    1
+    >>> ans = twenty_twenty_two(9)
+    >>> ans
+    1
+    >>> ans = twenty_twenty_two(80)
+    >>> 10
     """
-    return 2000+1+3*7
+    return a // 7
 
-if __name__ == '__main__':
-    print(9+8)
+
+new_res = twenty_twenty_two(71)
+print(new_res)
+
+
