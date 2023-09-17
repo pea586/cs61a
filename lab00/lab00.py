b75392d6dee70869a3ebc41715df45d90e523e1f
Bill <keepgoingbill@gmail.com>
2023-09-17 15:01:03 +1000

    Initial commit

---
+++ lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
+    using only numbers and the +, *, and - operators.
+
+    ''>>> twenty_twenty_two()
+    2022
+    """
+    return 2000+1+3*7
+
+if __name__ == '__main__':
+    print(9+8)
