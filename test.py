0ec4d28e8bcf8b5a570419970fc0550e379ede40
Bill <keepgoingbill@gmail.com>
2023-09-20 15:47:38 +1000

    'Sep20, Sound'

--- test.py
+++ test.py
@@ -1,62 +1,95 @@
 """
-Environments for Higher-order Func
+Test codes for the cs51a
 """
 
+from wave import open
+from struct import Struct
+from math import floor
 
-def apply_twice(f, x):
-    return f(f(x))
-
-
-# def square(x):
-#     return x ** 2
+frame_rate = 11025
 
 
-def add(a, b, c):
-    return a + b + c
+def encode(x):
+    """Encode float x between -1 and 1 as two bytes"""
+    i = int(16384 * x)
+    return Struct('h').pack(i)
 
 
-def currying_add(func):
-    def wrapper(a, c, b=666):
-        return func(a, b, c)
-
-    return wrapper
-
-
-# result = currying_add(add)(1, 2)
-# print(result)
-
-from operator import add, mul
-
-square = lambda x: x * x
-
-identity = lambda x: x
-
-triple = lambda x: 3 * x
-
-increment = lambda x: x + 1
+def play(sampler, name='song.wav', seconds=2):
+    """Write an output of a sampler func as a wav file"""
+    out = open(name, 'wb')
+    out.setnchannels(1)
+    out.setsampwidth(2)
+    out.setframerate(frame_rate)
+    t = 0
+    while t < seconds * frame_rate:
+        sample = sampler(t)
+        out.writeframes(encode(sample))
+        t += 1
+    out.close()
 
 
-def product(n, term):
-    start = 1
-    ans = 1
-    while start <= n:
-        ans *= term(start)
-        start += 1
-    return ans
+def tri(frequency, amplitude=0.3):
+    """A continuous triangle wave"""
+    period = frame_rate // frequency
+
+    def sampler(t):
+        saw_wave = t / period - floor(t / period + 0.5)
+        tri_wave = 2 * abs(2 * saw_wave) - 1
+        return amplitude * tri_wave
+
+    return sampler
 
 
-def accumulate(merger, start, n, term):
-    step = 1
-    while step <= n:
-        curr = merger(start, term(step))
-        start = curr
-        step += 1
-    return start
+c_freq = 261.63
+e_freq = 329.63
+g_freq = 392.00
 
 
-def product_using_accumulate(n, term):
-    return product(n, term)
+def both(f, g):
+    return lambda x: f(x) + g(x)
 
 
-ans = product_using_accumulate(6, triple)
-print(ans)
+def note(f, start, end, fade=0.1):
+    def sampler(t):
+        seconds = t / frame_rate
+        if seconds < start:
+            return 0
+        elif seconds > end:
+            return 0
+        elif seconds < start + fade:
+            return (seconds - start) / fade * f(t)
+        elif seconds > end - fade:
+            return (end - seconds) / fade * f(t)
+        else:
+            return f(t)
+
+    return sampler
+
+
+def mario_at(octave):
+    c, e = tri(octave * c_freq), tri(octave * e_freq)
+    g, low_g = tri(octave * g_freq), tri(octave * g_freq / 2)
+    return mario(c, e, g, low_g)
+
+
+def mario(c, e, g, low_g):
+    z = 0
+    song = note(e, z, z + 1 / 8)
+    z += 1 / 8
+    song = both(song, note(e, z, z + 1 / 8))
+    z += 1 / 4
+    song = both(song, note(e, z, z + 1 / 8))
+    z += 1 / 4
+    song = both(song, note(c, z, z + 1 / 8))
+    z += 1 / 8
+    song = both(song, note(e, z, z + 1 / 8))
+    z += 1 / 4
+    song = both(song, note(g, z, z + 1 / 8))
+    z += 1 / 2
+    song = both(song, note(low_g, z, z + 1 / 8))
+    z += 1 / 2
+    return song
+
+
+play(both(mario_at(1), mario_at(1 / 2)))
