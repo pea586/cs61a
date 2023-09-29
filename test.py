f4874cae8b143ee8a17834a0e73295c704c5521d
Bill <keepgoingbill@gmail.com>
2023-09-29 23:48:14 +1000

    'Sep29, Environment diagram again'

--- test.py
+++ test.py
@@ -1,95 +1,16 @@
 """
-Test codes for the cs51a
+it is usually good to use short name wisely
+n,k,i -> integers
+x,y,z -> real numbers
+f,g,h -> functions
 """
 
-from wave import open
-from struct import Struct
-from math import floor
-
-frame_rate = 11025
+a = lambda x: x * 2 + 1
 
 
-def encode(x):
-    """Encode float x between -1 and 1 as two bytes"""
-    i = int(16384 * x)
-    return Struct('h').pack(i)
+def b(b, x):
+    return b(x + a(x))
 
 
-def play(sampler, name='song.wav', seconds=2):
-    """Write an output of a sampler func as a wav file"""
-    out = open(name, 'wb')
-    out.setnchannels(1)
-    out.setsampwidth(2)
-    out.setframerate(frame_rate)
-    t = 0
-    while t < seconds * frame_rate:
-        sample = sampler(t)
-        out.writeframes(encode(sample))
-        t += 1
-    out.close()
-
-
-def tri(frequency, amplitude=0.3):
-    """A continuous triangle wave"""
-    period = frame_rate // frequency
-
-    def sampler(t):
-        saw_wave = t / period - floor(t / period + 0.5)
-        tri_wave = 2 * abs(2 * saw_wave) - 1
-        return amplitude * tri_wave
-
-    return sampler
-
-
-c_freq = 261.63
-e_freq = 329.63
-g_freq = 392.00
-
-
-def both(f, g):
-    return lambda x: f(x) + g(x)
-
-
-def note(f, start, end, fade=0.1):
-    def sampler(t):
-        seconds = t / frame_rate
-        if seconds < start:
-            return 0
-        elif seconds > end:
-            return 0
-        elif seconds < start + fade:
-            return (seconds - start) / fade * f(t)
-        elif seconds > end - fade:
-            return (end - seconds) / fade * f(t)
-        else:
-            return f(t)
-
-    return sampler
-
-
-def mario_at(octave):
-    c, e = tri(octave * c_freq), tri(octave * e_freq)
-    g, low_g = tri(octave * g_freq), tri(octave * g_freq / 2)
-    return mario(c, e, g, low_g)
-
-
-def mario(c, e, g, low_g):
-    z = 0
-    song = note(e, z, z + 1 / 8)
-    z += 1 / 8
-    song = both(song, note(e, z, z + 1 / 8))
-    z += 1 / 4
-    song = both(song, note(e, z, z + 1 / 8))
-    z += 1 / 4
-    song = both(song, note(c, z, z + 1 / 8))
-    z += 1 / 8
-    song = both(song, note(e, z, z + 1 / 8))
-    z += 1 / 4
-    song = both(song, note(g, z, z + 1 / 8))
-    z += 1 / 2
-    song = both(song, note(low_g, z, z + 1 / 8))
-    z += 1 / 2
-    return song
-
-
-play(both(mario_at(2), mario_at(1 / 2)))
+x = 3
+x = b(a, x)
