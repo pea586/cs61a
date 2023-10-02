fcb3681772fdd8cbfe889a0f346467345b6e57c2
Bill <keepgoingbill@gmail.com>
2023-10-02 11:10:02 +1100

    'Oct2, Function Examples'

--- test.py
+++ test.py
@@ -4,13 +4,43 @@ n,k,i -> integers
 x,y,z -> real numbers
 f,g,h -> functions
 """
-
-a = lambda x: x * 2 + 1
-
-
-def b(b, x):
-    return b(x + a(x))
+import collections
 
 
-x = 3
-x = b(a, x)
+def trace1(fn):
+    def traced(x):
+        print('Calling', fn, 'on argument', x)
+        return fn(x)
+
+    return traced
+
+
+@trace1
+def square(x):
+    return x * x
+
+
+@trace1
+def triple(x):
+    return x * 3
+
+
+# print(triple(5))
+
+def increment(x):
+    return x + 1
+
+
+def double(x):
+    return double(x + x)
+
+
+first = double
+
+
+def double(y):
+    return y + y
+
+
+result = first(10)
+print(result)  # 40
